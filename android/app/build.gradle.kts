847a34afb6989a5603ec0047d6cc2da6520a8dec
heba abdelhamid <hebaabdelhamidabdelazim@gmail.com>
2026-02-07 07:58:53 -0600

    feat:condition in cart vendor

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         applicationId = "com.jomlah.android"
         minSdk = flutter.minSdkVersion
         targetSdk = 36
-        versionCode = 14
+        versionCode = 15
         versionName = flutter.versionName
         multiDexEnabled = true
     }
